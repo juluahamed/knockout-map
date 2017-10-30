7c8fe08b086aa5ce160387cb20adcb58883f67bb
Julu Ahamed <juluahamed@gmail.com>
2017-10-30 13:25:28 +0530

    Add address to infoWindow

--- js/app.js
+++ js/app.js
@@ -42,6 +42,7 @@ function Location(l_data) {
 	self.website = 'N/A';
 	self.checkin = 'N/A';
 	self.picture = '';
+	self.address = 'N/A';
 
 	self.active = ko.observable(true);
 
@@ -79,16 +80,20 @@ function Location(l_data) {
 	}
 
 	self.infoWindowContentCreator = function() {
-		var content_string = "<p class='text-center'><strong>"+self.name+"</strong></p><p>Website :<a href="+ self.website +">"+ self.website+ "</a></p><p>Total Checkin :"+ self.checkin+"</p>"+ "<img class='displayImage' src="+self.picture+">";
-		console.log(content_string);
-		console.log(self.picture);
+		var content_string = "<p class='text-center'><strong>"+self.name+
+							"</strong></p><p>Website : <a href="+ self.website +">"+
+							 self.website+ "</a></p><p>Address : "+ self.address + 
+							 "</p><p>Total Checkin :"+ self.checkin+"</p>"+
+							 "<img class='displayImage' src="+self.picture+">";
 		self.infoWindow.setContent(content_string);
 			
 	}
 
 	//Foursquare API : Search for location and get photos
 
-	var fs_search_url = "https://api.foursquare.com/v2/venues/search?ll=" + self.lat + "," + self.lng + "&client_id=" + CLIENT_ID + '&client_secret=' + CLIENT_SECRET+ "&v=20171029"+ "&query=" +this.name;
+	var fs_search_url = "https://api.foursquare.com/v2/venues/search?ll=" + self.lat + "," +
+						 self.lng + "&client_id=" + CLIENT_ID + '&client_secret=' + CLIENT_SECRET+
+						  "&v=20171029"+ "&query=" +this.name;
 	
 	//Fire Ajax to get location info and ID
 	function ajaxSearch() {
@@ -96,14 +101,19 @@ function Location(l_data) {
 	        dataType: "json",
 			url: fs_search_url});
 	};
-
 	ajaxSearch().done(function(result) {
 		console.log("We are successful");
+		console.log(result.response.venues[0].location['address']);
 		var data = result.response.venues[0];
 	    self.id = data.id;
 		self.website = data.url;
 		self.checkin = data.stats['checkinsCount'];
-		var fs_search_photos = "https://api.foursquare.com/v2/venues/" + self.id +"/photos?client_id=" + CLIENT_ID + '&client_secret=' + CLIENT_SECRET+ "&v=20171029";
+		self.address = data.location['address'];
+		
+		var fs_search_photos = "https://api.foursquare.com/v2/venues/" + self.id +
+								"/photos?client_id=" + CLIENT_ID + '&client_secret=' 
+								+ CLIENT_SECRET+ "&v=20171029";
+		
 		// From above recieved ID, retrieve pictures of the location
 		function ajaxPhoto() {
 		    return $.ajax({
@@ -111,13 +121,15 @@ function Location(l_data) {
 				url: fs_search_photos});
 		};
 		ajaxPhoto().done(function(p_result){
-			self.picture = p_result.response.photos.items[0].prefix + "220x200"+p_result.response.photos.items[0].suffix;
+			self.picture = p_result.response.photos.items[0].prefix + "220x200"+
+							p_result.response.photos.items[0].suffix;
 			self.infoWindowContentCreator();
 		}).fail(function(){
 			self.picture = "><p>Could Not fetch pictures from Foursquare. Try Again</p";
 			self.infoWindowContentCreator();
 		});
 	}).fail(function() {
+		self.address = 'Unable to reach Foursquare. Try again later';
 	    self.website = '# Unable to reach Foursquare. Try again later';
 	    self.checkin = 'Unable to reach Foursquare. Try again later';
 	    self.infoWindowContentCreator();
@@ -135,12 +147,10 @@ function ListViewModel() {
 	self.sideMenu = ko.observable(false);
 	self.sideMenuCompute = ko.computed(function(){
 		if (self.sideMenu() == false) {
-			console.log("Closed. Opening");
 			document.getElementById("sideMenu").style.display = "block";
 	    	document.getElementById("wrapper").style.marginLeft = "250px";
 	    }
     	else {
-    		console.log("Open. Closing");
 		 	document.getElementById("sideMenu").style.display = "none";
 		 	document.getElementById("wrapper").style.marginLeft = "0";
     	}
@@ -191,7 +201,8 @@ function ListViewModel() {
 
 
 	// Search Logic : after each key press, take the input string, and use indexOf function
-	// to see if if input text so far is present in any of the location names. If yes, set 'active' property  as true
+	// to see if if input text so far is present in any of the location names. If yes, set 'active' property 
+	// as true
 	self.search = function(value) {
 		self.renderAllLocations(false);
 		if (value == '') {
